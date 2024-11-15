c4f3b571288b7436a818e4c7dd71c37e28a108ea
Serge <sergiusz.kozubek@gmail.com>
2024-11-15 16:29:12 +0100

    small bug fix

--- src/components/TradeList.tsx
+++ src/components/TradeList.tsx
@@ -85,7 +85,7 @@ const TradeList: React.FC<TradeListProps> = ({
             </div>
 
             {onTradeDelete && (
-              <div className="absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <div className="absolute bottom-2 right-2 opacity-100 transition-opacity">
                 <Button
                   variant="ghost"
                   size="sm"
